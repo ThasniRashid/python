beecb2cc230c2d2d0add3d84693fe9fe4f09e69e
thasnirashid <thasnisaleem1999@gmail.com>
2022-01-17 12:55:37 +0530

    commit

--- armstrong.py
+++ armstrong.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 n=int(input("Enter a number:"))
 def digit(s):
     sum=l=0
@@ -13,19 +13,4 @@ def digit(s):
         print( temp,"not armstrong")
         
 digit(n)
-=======
-n=int(input("Enter a number:"))
-def digit(s):
-    sum=l=0
-    temp=s
-    while(s>0):
-        l=s%10
-        sum=sum+(l**3)
-        s=s//10
-    if temp==sum:
-        print(temp,"armstrong")
-    else:
-        print( temp,"not armstrong")
-        
-digit(n)
->>>>>>> 7941df611bd30af9c0dd415b7b0a22f5843dbd2b
+
