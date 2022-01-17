7f8804e5a9aa1930d92b26040c39262b0dcd3082
thasnirashid <thasnisaleem1999@gmail.com>
2022-01-17 12:01:05 +0530

    commit

---
+++ neon.py
@@ -0,0 +1,15 @@
+a=int(input("Enter a value: "))
+def neon(x):
+    sqr=(x**2)
+    temp=x
+    sum=l=0
+    while(temp>0):
+        l=temp%10
+        sum=sum+l
+        temp=temp//10
+
+    if sum==x:
+        print(x,"is a neon")
+    else:
+        print(x,"is not neon")
+neon(a)
